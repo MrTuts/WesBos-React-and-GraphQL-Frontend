3d7ef774b0422b36292341f596663e45ec8769a3
Pavel <pavelgric@gmail.com>
2021-06-04 08:32:17 +0200

    39 - Implement sign up

--- components/SignUp.tsx
+++ components/SignUp.tsx
@@ -7,73 +7,75 @@ import Error from './ErrorMessage';
 import { CURRENT_USER_QUERY } from './User';
 import Form from './styles/Form';
 
-const SIGNIN_MUTATION = gql`
-  mutation SIGNIN_MUTATION($email: String!, $password: String!) {
-    authenticateUserWithPassword(email: $email, password: $password) {
-      ... on UserAuthenticationWithPasswordSuccess {
-        item {
-          id
-          email
-          name
-        }
-      }
-      ... on UserAuthenticationWithPasswordFailure {
-        code
-        message
-      }
+const SIGNUP_MUTATION = gql`
+  mutation SIGNUP_MUTATION(
+    $name: String!
+    $email: String!
+    $password: String!
+  ) {
+    createUser(data: { name: $name, email: $email, password: $password }) {
+      id
+      name
+      email
     }
   }
 `;
 
-type SingInData = {
-  authenticateUserWithPassword:
-    | {
-        __typename: 'UserAuthenticationWithPasswordSuccess';
-        item: {
-          id: string;
-          email: string;
-          name: string;
-        };
-      }
-    | {
-        __typename: 'UserAuthenticationWithPasswordFailure';
-        code: string;
-        message: string;
-      };
+type SingUpData = {
+  createUser: {
+    id: string;
+    email: string;
+    name: string;
+  };
 };
 
-export default function SignIn() {
+export default function SignUp() {
   const { inputs, handleChange, resetForm } = useForm({
     email: '',
+    name: '',
     password: '',
   });
 
-  const [signin, { data, loading }] = useMutation<SingInData>(SIGNIN_MUTATION, {
-    variables: inputs,
-    // refetch the currently logged in used
-    refetchQueries: [{ query: CURRENT_USER_QUERY }],
-  });
+  const [signup, { data, loading, error }] = useMutation<SingUpData>(
+    SIGNUP_MUTATION,
+    {
+      variables: inputs,
+      // refetch the currently logged in used
+      refetchQueries: [{ query: CURRENT_USER_QUERY }],
+    }
+  );
 
   const handleSubmit: FormEventHandler<HTMLFormElement> = async (e) => {
     e.preventDefault();
-    await signin();
+    // eslint-disable-next-line no-console
+    await signup().catch(console.error);
     resetForm();
   };
-
-  const error =
-    data?.authenticateUserWithPassword.__typename ===
-    'UserAuthenticationWithPasswordFailure'
-      ? data?.authenticateUserWithPassword
-      : undefined;
 
   return (
     <Form
       method="POST" // prevent putting inputted values into url (password)
       onSubmit={handleSubmit}
     >
-      <h2>Sign Into Your Account</h2>
+      <h2>Sign Up For an Account</h2>
       <Error error={error} />
       <fieldset disabled={loading} aria-busy={loading}>
+        {data?.createUser && (
+          <p>
+            Signed up with {data.createUser.email} - Please go ahead nd Sign in!
+          </p>
+        )}
+        <label htmlFor="name">
+          Your Name
+          <input
+            type="text"
+            name="name"
+            placeholder="Your name"
+            autoComplete="name"
+            value={inputs.name}
+            onChange={handleChange}
+          />
+        </label>
         <label htmlFor="email">
           Email
           <input
@@ -97,7 +99,7 @@ export default function SignIn() {
           />
         </label>
       </fieldset>
-      <button type="submit">Sing In!</button>
+      <button type="submit">Sing Up!</button>
     </Form>
   );
 }
