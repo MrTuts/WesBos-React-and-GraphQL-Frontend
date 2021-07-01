2089858c9113a331b0fdfd09f261f78afec827da
Pavel <pavelgric@gmail.com>
2021-07-01 09:06:53 +0200

    60 - Display all orders

--- pages/orders.tsx
+++ pages/orders.tsx
@@ -1,7 +1,127 @@
+import { useQuery } from '@apollo/client';
+import gql from 'graphql-tag';
+import Head from 'next/head';
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+import styled from 'styled-components';
+
+import ErrorMessage from '../components/ErrorMessage';
+import OrderItemStyles from '../components/styles/OrderItemStyles';
+import formatMoney from '../lib/formatMoney';
+
+const USER_ORDERS_QUERY = gql`
+  query USER_ORDERS_QUERY {
+    allOrders {
+      id
+      charge
+      total
+      user {
+        id
+      }
+      items {
+        id
+        name
+        description
+        price
+        quantity
+        photo {
+          image {
+            publicUrlTransformed
+          }
+        }
+      }
+    }
+  }
+`;
+
+type Order = {
+  id: string;
+  charge: string;
+  total: number;
+  user: {
+    id: string;
+  };
+  items: Array<{
+    id: string;
+    name: string;
+    description: string;
+    price: number;
+    quantity: number;
+    photo: {
+      image: {
+        publicUrlTransformed: string;
+      };
+    };
+  }>;
+};
+
+type OrdersReturn = {
+  allOrders: Array<Order>;
+};
+
+const OrderUl = styled.ul`
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(350px, 1fr));
+  grid-gap: 4rem;
+`;
+
+function countItemsInOrder(order: Order) {
+  return order.items.reduce((acc, item) => acc + item.quantity, 0);
+}
+
 export default function OrdersPage() {
+  const { query } = useRouter();
+
+  const { data, error, loading } = useQuery<OrdersReturn>(USER_ORDERS_QUERY, {
+    variables: { id: query.id },
+  });
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <ErrorMessage error={error} />;
+
+  if (!data?.allOrders) {
+    return null;
+  }
+  const { allOrders } = data;
+
   return (
     <div>
-      <p>Orders!</p>
+      <Head>
+        <title>Your Orders ({allOrders.length})</title>
+      </Head>
+      <h2>You have {allOrders.length} orders!</h2>
+      <OrderUl>
+        {allOrders.map((order) => {
+          const totalItems = countItemsInOrder(order);
+          return (
+            <OrderItemStyles>
+              <Link href={`/order/${order.id}`}>
+                <a>
+                  <div className="order-meta">
+                    <p>
+                      {totalItems} Item{totalItems === 1 ? '' : 's'}
+                    </p>
+                    <p>
+                      {order.items.length} Product
+                      {order.items.length === 1 ? '' : 's'}
+                    </p>
+                    <p>{formatMoney(order.total)}</p>
+                  </div>
+                  <div className="images">
+                    {order.items.map((item) => (
+                      <img
+                        key={item.id}
+                        src={item.photo?.image?.publicUrlTransformed}
+                        alt={item.name}
+                      />
+                    ))}
+                  </div>
+                </a>
+              </Link>
+            </OrderItemStyles>
+          );
+        })}
+      </OrderUl>
     </div>
   );
 }
